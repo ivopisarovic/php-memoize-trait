001615d994646e3dceb353928291dc9d863db343
ivopisarovic <web@pisarovic.cz>
2018-09-21 20:55:27 +0200

    Support for parent::__call

--- src/Memoize.php
+++ src/Memoize.php
@@ -6,20 +6,28 @@ trait Memoize {
     private $memoizer;
 
     public function __call($method, $params) {
-        return $this->memoize($this->getMethod($method), $params);
+        $cachedMethod = $this->getCachedMethod($method);
+
+        if ($cachedMethod === null && is_callable(['parent', $method])) {
+            return parent::__call($method, $params);
+        } else {
+            return $this->memoize($cachedMethod, $params);
+        }
     }
 
-    public function memoize($method, $params) {
+    public function memoize($method, $params)
+    {
         if (!isset($this->memoizer)) {
             $this->memoizer = new Memoizer(new InMemoryMemoizerCache());
         }
         return $this->memoizer->memoize([$this, $method], $params);
     }
 
-    private function getMethod($method) {
+    private function getCachedMethod($method) {
         if (substr($method, 0, 1) === '_') {
             return substr($method, 1);
+        } else {
+            return null;
         }
-        return null;
     }
 }
